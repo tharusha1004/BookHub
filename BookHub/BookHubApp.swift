063e86fc420556991d5fb4e7a3e7cb9f1d84ae42
tharusha1004 <tharusha.astro@gmail.com>
2024-11-04 22:43:43 +0530

    Initial Commit

---
+++ BookHub/BookHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookHubApp.swift
+//  BookHub
+//
+//  Created by mac on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct BookHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookHubTests/BookHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookHubTests.swift
+//  BookHubTests
+//
+//  Created by mac on 2024-11-04.
+//
+
+import Testing
+@testable import BookHub
+
+struct BookHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
