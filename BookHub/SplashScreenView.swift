9b9e6c5872f95351c5516e9fa7c74ea4bdf246e1
tharusha1004 <tharusha.astro@gmail.com>
2024-11-09 12:38:06 +0530

    update splash screen with ease in animation. and  also after animation set navigation to the login view

--- BookHub/SplashScreenView.swift
+++ BookHub/SplashScreenView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
     var body: some View {
-        Image("BookHub")
-            .resizable()
-            .frame(width: 170.0, height: 30.0)
-            .scaledToFit()
-            
+        
+        if isActive {
+            LoginView()
+        }else{
+            VStack{
+                VStack{
+                    Image(systemName: "book.fill")
+                        .font(.system(size: 60))
+                        .foregroundColor(.blue)
+                    Text("BookHub")
+                        .font(.system(size: 40))
+                        .foregroundColor(.black.opacity(0.8))
+                        .bold()
+                        
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear{
+                    withAnimation (.easeIn(duration:1.2)){
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                    
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                    withAnimation{
+                        self.isActive = true
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    SplashScreenView()
+struct SplashScreenView_Previews:PreviewProvider{
+    static var previews: some View{
+        SplashScreenView()
+    }
 }
-
-
-
 
