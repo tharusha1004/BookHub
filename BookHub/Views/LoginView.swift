6f318e99489e83e5e4c9cb26e2d25f3f06b32f41
tharusha1004 <tharusha.astro@gmail.com>
2024-11-11 23:30:12 +0530

    Finalize the UI of the login screen view

--- BookHub/Views/LoginView.swift
+++ BookHub/Views/LoginView.swift
@@ -16,44 +16,116 @@ struct LoginView: View {
                 Color.white
                     .ignoresSafeArea(edges: .all)
                 
+                //Logo
                 VStack{
                     Image("BookHub")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 200, height: 200)
-                        .border(Color.red)
+                        .frame(width: 150, height: 150)
                     
-                    VStack{
-                        TextField("Email", text:$email)
-                            .font(.title)
-                        SecureField("Password", text: $password)
-                            .font(.title)
+                    //Login Text
+                    Text("Login")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    VStack(spacing: 16){
+                        //Email Textfield
+                        VStack(alignment: .leading){
+                            Text("Email")
+                                .font(.subheadline)
+                            
+                            TextField("Email", text:$email, prompt: Text("Enter your email"))
+                                .font(.subheadline)
+                                .textFieldStyle(.roundedBorder)
+                                .autocapitalization(.none)
+                        }
+                        
+                        //Password Textfield
+                        VStack(alignment: .leading){
+                            Text("Password")
+                                .font(.subheadline)
+                            
+                            SecureField("Password", text: $password, prompt: Text("Enter your password"))
+                                .font(.subheadline)
+                                .textFieldStyle(.roundedBorder)
+                        }
+                        
+                        //Forgot Password Button
+                        Button{
+                            print("TODO: Forgot Password")
+                        } label: {
+                            Text("Forgot Password")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .font(.subheadline)
+                                .foregroundColor(.blue)
+                        }
                     }
                     .padding()
-                    .border(Color.green)
                     
-                    VStack{
-                        Button("Sign in"){
-                            print("TODO: Sign in")
+                    VStack(spacing: 16){
+                        //Signin Button
+                        Button{
+                            print("email:\(email)")
+                            print("password:\(password)")
+                        } label: {
+                            Text("Sign in")
+                                .frame(maxWidth: .infinity)
+                                .font(.title3.bold())
                         }
-                        Button("Create an account"){
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(14)
+                        
+                        //Create Account Button
+                        Button{
                             print("TODO: Create account")
+                        } label: {
+                            Text("Create Account")
+                                .frame(maxWidth: .infinity)
+                                .font(.title3.bold())
                         }
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(14)
                     }
                     .padding()
-                    .border(Color.purple)
                     
-                    Button("Forgot Password"){
-                        print("TODO: Forgot Password")
+                    Spacer()
+                    
+                    //Other Options
+                    VStack(spacing: 8){
+                        //Sign in with Apple Button
+                        Button{
+                            print("TODO: Signin with Apple")
+                        } label: {
+                            Label("Sign in with Apple", systemImage: "applelogo")
+                                .frame(maxWidth: .infinity)
+                                .font(.title3.bold())
+                        }
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.black)
+                        .cornerRadius(14)
                     }
                     .padding()
-                    .border(Color.blue)
-                }
                 
+                    //Face Authentication
+                    Button{
+                        print("TODO: Face Authentication")
+                    } label: {
+                        Text("Face Authnetication")
+                            .frame(maxWidth: .infinity)
+                            .font(.subheadline)
+                            .foregroundColor(.blue)
+                    }
+                    .padding()
+                }
                 
             }
             
-            .navigationTitle("Login")
+            //.navigationTitle("Login")
         }
     }
 }
