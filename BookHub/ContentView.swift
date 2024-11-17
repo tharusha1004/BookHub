2f557224bbb9055191e130b3d1d9bf08e443d486
tharusha1004 <tharusha.astro@gmail.com>
2024-11-17 23:58:52 +0530

    intial commit

--- BookHub/ContentView.swift
+++ BookHub/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("This is Development Environment").bold()
+            Text("This is authentication").bold()
         }
         .padding()
     }
